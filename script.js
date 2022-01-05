a6a70bb584bcf7aa279b967a355a10ebc82408c8
Brandon Chu <brandoc318@gmail.com>
2022-01-04 21:06:59 -0800

    Roughed in solution for selecting name, X/O, and creating player objects.

--- script.js
+++ script.js
@@ -3,38 +3,59 @@ const gameBoard = (() => {
     let players = [];
     return {
         game,
+        players
     };
 })();
 
 const display = (() => {
-    const boxes = document.querySelectorAll(".box");
-    let activePlayer = true;
-    function displayMoves() {
-        for (let i = 0; i < 9; i++) {
-            boxes[i].textContent = gameBoard.game[i];
-        }
+    const _boxes = document.querySelectorAll(".box");
+    let _activePlayer = true;
+    function displayMoves(box, choice) {
+        box.textContent = choice;
+        gameBoard.game.push(choice);
+        return box;
     }
     function playerMoves(p1Choice, p2Choice) {
-        boxes.forEach((box) => {
+        _boxes.forEach((box) => {
             box.addEventListener("click", () => {
-                if (activePlayer && !box.classList.contains("taken")) {
-                    box.textContent = p1Choice;
-                    box.classList.add("taken");
-                    activePlayer = false;
+                if (_activePlayer && !box.classList.contains("taken")) {
+                    box = displayMoves(box, p1Choice);
+                    box.classList.add("taken", "p1-color");
+                    _activePlayer = false;
                     console.log("p1");
-                } else if (!activePlayer && !box.classList.contains("taken")) {
-                    box.textContent = p2Choice;
-                    box.classList.add("taken");
-                    activePlayer = true;
+                } else if (!_activePlayer && !box.classList.contains("taken")) {
+                    box = displayMoves(box, p2Choice);
+                    box.classList.add("taken", "p2-color");
+                    _activePlayer = true;
                     console.log("p2");
                 }
             });
         });
     }
+    function createPlayer(playerName, selection) {
+        const player = Player(playerName, selection);
+        gameBoard.players.push(player);
+    }
+    const p1Selection = document.querySelectorAll(".p1Selection");
+    p1Selection.forEach((sel) => {
+        sel.addEventListener("click", () => {
+            sel.setAttribute("id", "p1-sel")
+        });
+    });
+    const enterBtns = document.querySelectorAll(".submit-data");
+    enterBtns.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            const pName = document.getElementById("p1-name").value;
+            const sel = document.getElementById("p1-sel").textContent;
+            createPlayer(pName, sel);
+            console.log(pName, sel);
+        });
+    });
     return {
-        displayMoves,
         playerMoves,
     };
 })();
+
+const Player = (playerName, selection) => {};
 
 display.playerMoves("X", "O");
