602ef15bf119a70ea86f28e9f13bb630cc506290
Brandon Chu <brandoc318@gmail.com>
2022-01-06 15:49:07 -0800

    Implemented win/loss logic, currently only returns true when a win is determined. Should be able to significantly simiplify it afterward.

--- script.js
+++ script.js
@@ -4,52 +4,159 @@ const gameBoard = (() => {
     const boxes = document.querySelectorAll(".box");
     function gameDecider(num, playerClass) {
         if (num == 0 || num == 1 || num == 2) {
-            return horiDown(num, playerClass);
+            if (horiDown(num, playerClass)) {
+                return true;
+            }
+            if (num == 0) {
+                if (cross0(num, playerClass)) return true;
+            }
+            if (num == 2) {
+                if (cross2(num, playerClass)) return true;
+            }
         }
         if (num == 6 || num == 7 || num == 8) {
-            return horiUp(num, playerClass);
+            if (horiUp(num, playerClass)) {
+                return true;
+            }
+            if (num == 6) {
+                if (cross6(num,playerClass)) return true;
+            }
+            if (num == 8) {
+                if (cross8(num,playerClass)) return true;
+            }
         }
         if (num == 0 || num == 3 || num == 6) {
-            return vertR(num, playerClass);
+            if (vertR(num, playerClass)) return true;
         }
         if (num == 2 || num == 5 || num == 8) {
-            return vertL(num, playerClass);
+            if (vertL(num, playerClass)) return true;
+        }
+        if (num == 1 || num == 7) {
+            if (horiCenter(num, playerClass)) return true;
+        }
+        if (num == 3 || num == 5) {
+            if (vertCenter(num, playerClass)) return true;
         }
     }
-        function horiDown(num, playerClass) {
-            if (boxes[num].classList.contains(playerClass) && 
-            boxes[num + 3].classList.contains(playerClass) && 
-            boxes[num + 6].classList.contains(playerClass)) {
-                return true;
-            }
+    function horiDown(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num + 3].classList.contains(playerClass) &&
+            boxes[num + 6].classList.contains(playerClass)
+        ) {
+            return true;
         }
-        function horiUp(num, playerClass) {
-            if (boxes[num].classList.contains(playerClass) && 
-            boxes[num - 3].classList.contains(playerClass) && 
-            boxes[num - 6].classList.contains(playerClass)) {
-                return true;
-            }
+        return false;
+    }
+    function horiUp(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num - 3].classList.contains(playerClass) &&
+            boxes[num - 6].classList.contains(playerClass)
+        ) {
+            return true;
         }
-        function vertR(num, playerClass) {
-            if (boxes[num].classList.contains(playerClass) && 
-            boxes[num + 1].classList.contains(playerClass) && 
-            boxes[num + 2].classList.contains(playerClass)) {
-                return true;
-            }
+        return false;
+    }
+    function vertR(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num + 1].classList.contains(playerClass) &&
+            boxes[num + 2].classList.contains(playerClass)
+        ) {
+            return true;
         }
-        function vertL(num, playerClass) {
-            if (boxes[num].classList.contains(playerClass) && 
-            boxes[num - 1].classList.contains(playerClass) && 
-            boxes[num - 2].classList.contains(playerClass)) {
-                return true;
-            }
+        return false;
+    }
+    function vertL(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num - 1].classList.contains(playerClass) &&
+            boxes[num - 2].classList.contains(playerClass)
+        ) {
+            return true;
         }
+        return false;
+    }
+    function cross2(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num + 2].classList.contains(playerClass) &&
+            boxes[num + 4].classList.contains(playerClass)
+        ) {
+            return true;
+        }
+        return false;
+    }
+    function cross6(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num - 2].classList.contains(playerClass) &&
+            boxes[num - 4].classList.contains(playerClass)
+        ) {
+            return true;
+        }
+        return false;
+    }
+    function cross0(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num + 4].classList.contains(playerClass) &&
+            boxes[num + 8].classList.contains(playerClass)
+        ) {
+            return true;
+        }
+        return false;
+    }
+    function cross8(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num - 4].classList.contains(playerClass) &&
+            boxes[num - 8].classList.contains(playerClass)
+        ) {
+            return true;
+        }
+        return false;
+    }
+    function horiCenter(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num + 1].classList.contains(playerClass) &&
+            boxes[num - 1].classList.contains(playerClass)
+        ) {
+            return true;
+        }
+        return false;
+    }
+    function vertCenter(num, playerClass) {
+        if (
+            boxes[num].classList.contains(playerClass) &&
+            boxes[num + 3].classList.contains(playerClass) &&
+            boxes[num - 3].classList.contains(playerClass)
+        ) {
+            return true;
+        }
+        return false;
+    }
     return {
         game,
         players,
         gameDecider,
     };
 })();
+
+const Player = (playerName, selection) => {
+    function getName() {
+        return playerName;
+    }
+    function getSelection() {
+        return selection;
+    }
+    return {
+        getSelection,
+        getName,
+    };
+};
 
 const display = (() => {
     const _boxes = document.querySelectorAll(".box");
@@ -60,25 +167,29 @@ const display = (() => {
         return box;
     }
     function playerMoves(p1Choice, p2Choice) {
-        let count = 0;
-        _boxes.forEach((box) => {
-            box.addEventListener("click", () => {
-                if (_activePlayer && !box.classList.contains("taken")) {
-                    box = displayMoves(box, p1Choice);
-                    box.classList.add("taken", "p1-color");
+        let color;
+        for (let i = 0; i < _boxes.length; i++) {
+            _boxes[i].addEventListener("click", () => {
+                if (_activePlayer && !_boxes[i].classList.contains("taken")) {
+                    _boxes[i] = displayMoves(_boxes[i], p1Choice);
+                    _boxes[i].classList.add("taken", "p1-color");
                     _activePlayer = false;
-                    console.log(gameBoard.gameDecider(count, "p1-color"));
+                    color = "p1-color";
                     // console.log("p1");
-                } else if (!_activePlayer && !box.classList.contains("taken")) {
-                    box = displayMoves(box, p2Choice);
-                    box.classList.add("taken", "p2-color");
+                } else if (
+                    !_activePlayer &&
+                    !_boxes[i].classList.contains("taken")
+                ) {
+                    _boxes[i] = displayMoves(_boxes[i], p2Choice);
+                    _boxes[i].classList.add("taken", "p2-color");
                     _activePlayer = true;
-                    console.log(gameBoard.gameDecider(count, "p2-color"));
+                    color = "p2-color";
                     // console.log("p2");
                 }
-                count++;
+                console.log(i);
+                console.log(gameBoard.gameDecider(i, color));
             });
-        });
+        }
     }
     function createPlayer(playerName, selection) {
         const player = Player(playerName, selection);
@@ -88,9 +199,10 @@ const display = (() => {
             let p2 = gameBoard.players[1].getSelection();
             if (p1 != p2) {
                 display.playerMoves(p1, p2);
-            } 
+            }
         }
     }
+
     const selection = document.querySelectorAll(".selection");
     let activeP1;
     let activeP2;
@@ -128,18 +240,8 @@ const display = (() => {
     });
     return {
         playerMoves,
+        createPlayer,
     };
 })();
-
-const Player = (playerName, selection) => {
-    function getName() {
-        return playerName;
-    }
-    function getSelection() {
-        return selection;
-    }
-    return {
-        getSelection,
-        getName,
-    };
-};
+const testPlayer1 = display.createPlayer("brandon", "X");
+const testPlayer2 = display.createPlayer("brando", "O");
