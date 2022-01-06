c90f5e206fc1731fd8910c930911c9252c9abb29
Brandon Chu <brandoc318@gmail.com>
2022-01-05 23:05:13 -0800

    Created foundation for win/loss logic using the various winning possibilties.

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
 const gameBoard = (() => {
-    let game = ["x", "o", "o", "o", "x", "x", "o", "o", "x"];
+    let game = [];
     let players = [];
+    const boxes = document.querySelectorAll(".box");
+    function gameDecider(num, playerClass) {
+        if (num == 0 || num == 1 || num == 2) {
+            return horiDown(num, playerClass);
+        }
+        if (num == 6 || num == 7 || num == 8) {
+            return horiUp(num, playerClass);
+        }
+        if (num == 0 || num == 3 || num == 6) {
+            return vertR(num, playerClass);
+        }
+        if (num == 2 || num == 5 || num == 8) {
+            return vertL(num, playerClass);
+        }
+    }
+        function horiDown(num, playerClass) {
+            if (boxes[num].classList.contains(playerClass) && 
+            boxes[num + 3].classList.contains(playerClass) && 
+            boxes[num + 6].classList.contains(playerClass)) {
+                return true;
+            }
+        }
+        function horiUp(num, playerClass) {
+            if (boxes[num].classList.contains(playerClass) && 
+            boxes[num - 3].classList.contains(playerClass) && 
+            boxes[num - 6].classList.contains(playerClass)) {
+                return true;
+            }
+        }
+        function vertR(num, playerClass) {
+            if (boxes[num].classList.contains(playerClass) && 
+            boxes[num + 1].classList.contains(playerClass) && 
+            boxes[num + 2].classList.contains(playerClass)) {
+                return true;
+            }
+        }
+        function vertL(num, playerClass) {
+            if (boxes[num].classList.contains(playerClass) && 
+            boxes[num - 1].classList.contains(playerClass) && 
+            boxes[num - 2].classList.contains(playerClass)) {
+                return true;
+            }
+        }
     return {
         game,
         players,
+        gameDecider,
     };
 })();
 
@@ -16,19 +60,23 @@ const display = (() => {
         return box;
     }
     function playerMoves(p1Choice, p2Choice) {
+        let count = 0;
         _boxes.forEach((box) => {
             box.addEventListener("click", () => {
                 if (_activePlayer && !box.classList.contains("taken")) {
                     box = displayMoves(box, p1Choice);
                     box.classList.add("taken", "p1-color");
                     _activePlayer = false;
-                    console.log("p1");
+                    console.log(gameBoard.gameDecider(count, "p1-color"));
+                    // console.log("p1");
                 } else if (!_activePlayer && !box.classList.contains("taken")) {
                     box = displayMoves(box, p2Choice);
                     box.classList.add("taken", "p2-color");
                     _activePlayer = true;
-                    console.log("p2");
+                    console.log(gameBoard.gameDecider(count, "p2-color"));
+                    // console.log("p2");
                 }
+                count++;
             });
         });
     }
@@ -36,10 +84,11 @@ const display = (() => {
         const player = Player(playerName, selection);
         gameBoard.players.push(player);
         if (gameBoard.players.length == 2) {
-            display.playerMoves(
-                gameBoard.players[0].getSelection(),
-                gameBoard.players[1].getSelection()
-            );
+            let p1 = gameBoard.players[0].getSelection();
+            let p2 = gameBoard.players[1].getSelection();
+            if (p1 != p2) {
+                display.playerMoves(p1, p2);
+            } 
         }
     }
     const selection = document.querySelectorAll(".selection");
@@ -83,10 +132,14 @@ const display = (() => {
 })();
 
 const Player = (playerName, selection) => {
+    function getName() {
+        return playerName;
+    }
     function getSelection() {
         return selection;
     }
     return {
         getSelection,
+        getName,
     };
 };
